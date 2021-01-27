12590b17ce785662987e6bda5cefb7d9cf739e1e
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2021-01-27 20:40:25 +0900

    Create 3-3-1.py

---
+++ Algorithm/3-3-1.py
@@ -0,0 +1,14 @@
+n, m = list(map(int, input().split()))
+
+result = 0
+
+for i in range(n):
+  data = list(map(int, input().split()))
+
+  min_value = 10001
+  for j in data:
+    min_value = min(min_value, j)
+  
+  result = max(result, min_value)
+
+print(result)
